63976efc2f71530414a4d31b21d629b516366c61
G-BAT <batman02970@gmail.com>
2025-08-05 12:44:30 +0530

    added some code

--- recursion/printing all subsets in an array.cpp
+++ recursion/printing all subsets in an array.cpp
@@ -17,7 +17,7 @@ void subsetSumK(vector<int>& arr, int i, vector<int>& current, vector<vector<int
 }
 
 int main() {
-    vector<int> arr = {1, 2, 1};
+    vector<int> arr = {1, 2, 3};
     vector<vector<int>> result;
     vector<int> current;
     subsetSumK(arr, 0, current, result);
@@ -30,3 +30,48 @@ int main() {
         cout << "}";
     }
 }
+
+/////////////////////////////////////////////////////
+
+//subset with duplicates 
+
+// #include <iostream>
+// #include <algorithm>
+// #include <vector>
+// using namespace std;
+
+// void subsetSumK(vector<int>& arr, int i, vector<int>& current, vector<vector<int>> &result) {
+
+//     if(i==arr.size()){
+//         result.push_back(current);
+//         return;
+//     }
+
+//     current.push_back(arr[i]);
+//     subsetSumK(arr,i+1,current,result);
+//     current.pop_back();
+
+//     int ind=i+1;
+//     while(ind<arr.size() && arr[ind]==arr[ind-1]){
+//         ++ind;
+//     }
+
+//     subsetSumK(arr,ind,current,result);
+// }
+
+// int main() {
+//     vector<int> arr = {1, 2, 1};
+//     vector<vector<int>> result;
+//     vector<int> current;
+//     sort(arr.begin(), arr.end());
+
+//     subsetSumK(arr, 0, current, result);
+
+//     for(auto &i:result){
+//         cout << "{";
+//         for(int j:i){
+//             cout << j << " ";
+//         }
+//         cout << "}";
+//     }
+// }
